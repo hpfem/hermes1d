3c75ed8ef67558dd71294af21b24341d2af1c7f5
Ondrej Certik <ondrej@certik.cz>
2009-10-03 16:44:42 -0700

    Mesh::assign_dofs() method added

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -24,3 +24,49 @@ void Mesh::set_poly_orders(int poly_order)
     this->elems[i].dof = new int[poly_order+1];
   }
 }
+
+void Mesh::assign_dofs()
+{
+  int DEBUG=1;
+  // define element connectivities
+  // (so far only for zero Dirichlet conditions)
+  // (a) enumerate vertex dofs
+  elems[0].dof[0] = -1;        // Dirichlet left end of domain
+  elems[0].dof[1] = 0;         // first vertex dof
+  for(int i=1; i<n_elem-1; i++) {
+    elems[i].dof[0] = i-1;     // Dirichlet left end of domain
+    elems[i].dof[1] = i;       // first dof
+  }
+  elems[n_elem-1].dof[0] = n_elem-2;     // last vertex dof
+  elems[n_elem-1].dof[1] = -1;      // Dirichlet left end of domain
+  // (b) enumerate bubble dofs
+  n_dof = n_elem-1; 
+  for(int i=0; i<n_elem; i++) {
+    for(int j=2; j<=elems[i].p; j++) {
+      elems[i].dof[j] = n_dof++;     // enumerating higher-order dofs
+    }
+  }
+
+  // test (print DOF in elements)
+  int n_test = 0;
+  for(int i=0; i<n_elem; i++) n_test += elems[i].p;
+  n_test -= 1;
+  if(n_dof != n_test) {
+    printf("n_dof = %d, n_test = %d\n", n_dof, n_test);
+    error("Internal: Test of total DOF number failed."); 
+  }
+
+  // test (print element connectivities)
+  if(DEBUG) {
+    printf("Printing element DOF arrays:\n");
+    printf("Elements = %d\n", n_elem);
+    printf("DOF = %d", n_dof);
+    for (int i = 0; i < n_elem; i++) {
+      printf("\nElement[%d]: ", i); 
+      for(int j = 0; j<elems[i].p+1; j++) {
+        printf("%d, ", elems[i].dof[j]);
+      }
+    }
+    printf("\n"); 
+  }
+}
--- src/mesh.h
+++ src/mesh.h
@@ -7,9 +7,11 @@ class Mesh {
     public:
         void create(double A, double B, int n);
         void set_poly_orders(int poly_order);
+        void assign_dofs();
 
     private:
         int n_elem;
+        int n_dof;
         Vertex *vertices;
         Element *elems;
 };
