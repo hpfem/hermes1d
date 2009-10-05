a4d69cb9f4172f25980fce238fb5f4ac25a4d587
Ondrej Certik <ondrej@certik.cz>
2009-10-04 22:45:28 -0700

    Make the example plot the sin(x) function

--- examples/laplace_bc_neumann/main.cpp
+++ examples/laplace_bc_neumann/main.cpp
@@ -8,7 +8,7 @@ int DEBUG = 1;
 // general input:
 static int NUM_EQ = 1;
 int Nelem = 30;                         // number of elements
-double A = 0, B = M_PI;              // domain end points
+double A = 0, B = 2*M_PI;              // domain end points
 int P_INIT = 1;                        // initial polynomal degree
 
 // boundary conditions
@@ -20,7 +20,7 @@ double TOL = 1e-8;
 
 // right-hand side
 double f(double x) {
-  return 1;
+  return sin(x);
   //return 1;
 }
 
@@ -79,16 +79,24 @@ double residual_vol(int num, double *x, double *weights,
   return val;
 };
 
-/*
-   We are adding a "-", because that is what is in the weak formulation. And we
-   are specifying the *normal* derivative, which means that is equal to the
-   regular derivative on the right hand side, but minus regular derivative on
-   the LHS.
-   */
 double residual_surf_right(double x, double u_prev, double du_prevdx,
         double v, double dvdx, void *user_data)
 {
-    // later this will be created from the user_data
+    // FIXME: Later, the value 'val_neum_right' will enter through user_data,
+    // not as a global variable
+    // NOTE: the minus sign here is due to the fact that the surface
+    // integral -\int_{\partial \Omega} \partial u/\partial nu times v
+    // has a negative sign in front of it. But the convention for 
+    // defining weak forms in Hermes1D is that all of them are with 
+    // positive signs. 
+    // NOTE: the Neumann boundary condition deals with the outer
+    // normal derivative. At the right end of the interval (a, b), this 
+    // is equal to the x-derivative, but at the left end point of (a, b)
+    // this is minus one times the x-derivative. In other words, if you 
+    // want your solution to be increasing with slope 1 at 'b', the normal
+    // derivative at 'b' needs to be 1. If you want the solution to be 
+    // increasing with slope 1 at 'a', then the normal derivative at 'a'
+    // needs to be -1.   
     return -val_neum_right * v; 
 }
 
