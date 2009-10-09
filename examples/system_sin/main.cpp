70b19e91b7dd00a2e58d41ef8227556bef51d2d8
Ondrej Certik <ondrej@certik.cz>
2009-10-08 23:05:20 -0700

    system_sin changed to solve u''+k^2u=0

--- examples/system_sin/main.cpp
+++ examples/system_sin/main.cpp
@@ -3,36 +3,26 @@
 
 // ********************************************************************
 // This example solves a system of two linear second-order equations 
-// - u'' + v - f_0 = 0
-// - v'' + u - f_1 = 0
-// in an interval (A, B) equipped with Dirichlet bdy conditions 
-// u(A) = exp(A), u(B) = exp(B), v(A) = exp(-A), v(B) = exp(-B). 
-// The exact solution is u(x) = exp(x), v(x) = exp(-x). 
+// u' + k^2 v = 0
+// u - v' = 0
+// which is equivalent to u'' + k^2 u = 0
+// in an interval (0, 2*pi) equipped with Dirichlet bdy conditions 
+// u(0) = 0, v(0) = k
+// The exact solution is u(x) = sin(k*x), v(x) = k*cos(k*x)
 
 // General input:
 static int N_eq = 2;
-int N_elem = 2;          // number of elements
-double A = 0, B = 1;     // domain end points
+int N_elem = 20;          // number of elements
+double A = 0, B = 2*M_PI;     // domain end points
 int P_init = 2;          // initial polynomal degree
+double k = 1.0;          // the constant in the equation
 
 // Tolerance for the Newton's method
 double TOL = 1e-5;
 
 // Boundary conditions
-double Val_dir_left_0 = exp(A);
-double Val_dir_right_0 = exp(B);
-double Val_dir_left_1 = exp(-A);
-double Val_dir_right_1 = exp(-B);
-
-// Function f_0(x)
-double f_0(double x) {
-  return -exp(x) + exp(-x);
-}
-
-// Function f_1(x)
-double f_1(double x) {
-  return -exp(-x) + exp(x);
-}
+double Val_dir_left_0 = 0;
+double Val_dir_left_1 = k;
 
 // ********************************************************************
 
@@ -46,7 +36,7 @@ double jacobian_0_0(int num, double *x, double *weights,
 {
   double val = 0;
   for(int i = 0; i<num; i++) {
-    val += dudx[i]*dvdx[i]*weights[i];
+    val += dudx[i]*v[i]*weights[i];
   }
   return val;
 };
@@ -59,7 +49,7 @@ double jacobian_0_1(int num, double *x, double *weights,
 {
   double val = 0;
   for(int i = 0; i<num; i++) {
-    val += u[i]*v[i]*weights[i];
+    val += k*k*u[i]*v[i]*weights[i];
   }
   return val;
 };
@@ -85,7 +75,7 @@ double jacobian_1_1(int num, double *x, double *weights,
 {
   double val = 0;
   for(int i = 0; i<num; i++) {
-    val += dudx[i]*dvdx[i]*weights[i];
+    val += -dudx[i]*v[i]*weights[i];
   }
   return val;
 };
@@ -97,8 +87,7 @@ double residual_0(int num, double *x, double *weights,
 {
   double val = 0;
   for(int i = 0; i<num; i++) {
-    val += (du_prevdx[0][i]*dvdx[i] + u_prev[1][i]*v[i]  
-           - f_0(x[i])*v[i])*weights[i];
+    val += (du_prevdx[0][i]*v[i] + k*k*u_prev[1][i]*v[i])*weights[i];
   }
   return val;
 };
@@ -110,8 +99,7 @@ double residual_1(int num, double *x, double *weights,
 {
   double val = 0;
   for(int i = 0; i<num; i++) {
-    val += (du_prevdx[1][i]*dvdx[i] + u_prev[0][i]*v[i]  
-           - f_1(x[i])*v[i])*weights[i];
+    val += (u_prev[0][i]*v[i] - du_prevdx[1][i]*v[i])*weights[i];
   }
   return val;
 };
@@ -123,9 +111,7 @@ int main() {
   mesh.create(A, B, N_elem);
   mesh.set_uniform_poly_order(P_init);
   mesh.set_bc_left_dirichlet(0, Val_dir_left_0);
-  mesh.set_bc_right_dirichlet(0, Val_dir_right_0);
   mesh.set_bc_left_dirichlet(1, Val_dir_left_1);
-  mesh.set_bc_right_dirichlet(1, Val_dir_right_1);
   int N_dof = mesh.assign_dofs();
   printf("N_dof = %d\n", N_dof);
 
