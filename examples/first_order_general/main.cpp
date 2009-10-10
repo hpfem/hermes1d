5734bb1e26df053fc4fbf13694515451d2076dea
Ondrej Certik <ondrej@certik.cz>
2009-10-09 23:38:19 -0700

    Added new example first_order_general.

---
+++ examples/first_order_general/main.cpp
@@ -0,0 +1,127 @@
+#include "hermes1d.h"
+#include "solver_umfpack.h"
+
+// ********************************************************************
+
+// This example solves the general first-order equation 
+// y' = f(y, x) in an interval (A, B), equipped with the 
+// initial condition y(A) = YA.
+
+// General input:
+static int N_eq = 1;                    // number of equations
+int N_elem = 3;                         // number of elements
+double A = 0, B = 10;                   // domain end points
+double YA = 1;                          // equation parameter
+int P_init = 2;                         // initial polynomal degree
+
+// Tolerance for the Newton's method
+double TOL = 1e-5;
+
+// Function f(y, x)
+double f(double y, double x) {
+  return -y;
+}
+
+// Function dfdy(y, x)
+double dfdy(double y, double x) {
+  return -1;
+}
+
+// ********************************************************************
+
+// bilinear form for the Jacobi matrix 
+// num...number of Gauss points in element
+// x[]...Gauss points
+// weights[]...Gauss weights for points in x[]
+// u...basis function
+// v...test function
+// u_prev...previous solution (all solution components)
+double jacobian(int num, double *x, double *weights, 
+                double *u, double *dudx, double *v, double *dvdx, 
+                double u_prev[MAX_EQN_NUM][MAX_PTS_NUM], 
+                double du_prevdx[MAX_EQN_NUM][MAX_PTS_NUM], 
+                void *user_data)
+{
+  double val = 0;
+  for(int i = 0; i<num; i++) {
+    val += (dvdx[i]*u[i] - dfdy(u_prev[i], x[i])*v[i]*u[i])*weights[i];
+  }
+  return val;
+};
+
+// (nonlinear) form for the residual vector
+// num...number of Gauss points in element
+// x[]...Gauss points
+// weights[]...Gauss weights for points in x[]
+// u...approximate solution
+// v...test function
+// u_prev...previous solution (all solution components)
+double residual(int num, double *x, double *weights, 
+                double u_prev[MAX_EQN_NUM][MAX_PTS_NUM], 
+                double du_prevdx[MAX_EQN_NUM][MAX_PTS_NUM],  
+                double *v, double *dvdx, void *user_data)
+{
+  double val = 0;
+  for(int i = 0; i<num; i++) {
+    val += (du_prevdx[0][i]*v[i] - f(u_prev[i], x[i])*v[i])*weights[i];
+  }
+  return val;
+};
+
+/******************************************************************************/
+int main() {
+  // create mesh
+  Mesh mesh(N_eq);
+  mesh.create(A, B, N_elem);
+  mesh.set_uniform_poly_order(P_init);
+  mesh.set_bc_left_dirichlet(0, YA);
+  int N_dof = mesh.assign_dofs();
+  printf("N_dof = %d\n", N_dof);
+
+  // register weak forms
+  DiscreteProblem dp(&mesh);
+  dp.add_matrix_form(0, 0, jacobian);
+  dp.add_vector_form(0, residual);
+
+  // allocate Jacobi matrix and residual
+  Matrix *mat;
+  double *y_prev = new double[N_dof];
+  double *res = new double[N_dof];
+
+  // zero initial condition for the Newton's method
+  for(int i=0; i<N_dof; i++) y_prev[i] = 0; 
+
+  // Newton's loop
+  while (1) {
+    // zero the matrix:
+    mat = new CooMatrix(N_dof);
+
+    // construct residual vector
+    dp.assemble_matrix_and_vector(mat, res, y_prev); 
+
+    // calculate L2 norm of residual vector
+    double res_norm = 0;
+    for(int i=0; i<N_dof; i++) res_norm += res[i]*res[i];
+    res_norm = sqrt(res_norm);
+
+    // if residual norm less than TOL, quit
+    // latest solution is in y_prev
+    if(res_norm < TOL) break;
+
+    // changing sign of vector res
+    for(int i=0; i<N_dof; i++) res[i]*= -1;
+
+    // solving the matrix system
+    solve_linear_system_umfpack((CooMatrix*)mat, res);
+
+    // updating y_prev by new solution which is in res
+    for(int i=0; i<N_dof; i++) y_prev[i] += res[i];
+  }
+
+  Linearizer l(&mesh);
+  const char *out_filename = "solution.gp";
+  l.plot_solution(out_filename, y_prev);
+
+  printf("Done.\n");
+  return 1;
+}
