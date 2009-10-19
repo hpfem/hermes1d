24155a5dc812e08f8d388128d7196233a0e75b69
Pavel Solin <solin@unr.edu>
2009-10-18 22:49:57 -0700

    adapt added

--- examples/system_sin_adapt/plot.py
+++ examples/system_sin_adapt/plot.py
@@ -1,4 +1,4 @@
-from pylab import plot, show
+from pylab import plot, show, legend
 import numpy
 data = numpy.loadtxt("solution.gp_0")
 x = data[:, 0]
@@ -8,4 +8,13 @@ data = numpy.loadtxt("solution.gp_1")
 x = data[:, 0]
 y = data[:, 1]
 plot(x, y, label="1")
+data = numpy.loadtxt("solution_ref.gp_0")
+x = data[:, 0]
+y = data[:, 1]
+plot(x, y, label="0 ref")
+data = numpy.loadtxt("solution_ref.gp_1")
+x = data[:, 0]
+y = data[:, 1]
+plot(x, y, label="1 ref")
+legend()
 show()
