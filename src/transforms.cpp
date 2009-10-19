cb5dbc64c44dcad9513b632de2c0811e1d89ebf3
Pavel Solin <solin@unr.edu>
2009-10-18 17:09:23 -0700

    phi implemented

--- src/transforms.cpp
+++ src/transforms.cpp
@@ -5,16 +5,38 @@
 
 #include "transforms.h"
 
-#define N_chebyshev (3+2*(P_MAX-1))
+#define N_chebyshev (3+2*(MAX_P-1))
 
-double chebyshev_points[N_chebyshev]
+double chebyshev_points[N_chebyshev];
 
 void calculate_chebyshev_points()
 {
-    for (int i; i < N_chebyshev, i++)
+    for (int i; i < N_chebyshev; i++)
         chebyshev_points[i] = cos(i*M_PI/(N_chebyshev-1));
 }
 
+// transform values from (-1, 0) to (-1, 1)
+#define map_left(x) (2*x+1)
+// transform values from (0, 1) to (-1, 1)
+#define map_right(x) (2*x-1)
+
 double phi(int i, double x)
 {
+    if (x < 0) {
+        if (i == 0)
+            return lobatto_fn_tab_1d[0](map_left(x));
+        else if (i % 2 == 0)
+            return 0;
+        else
+            return lobatto_fn_tab_1d[(i+1)/2](map_left(x));
+    } else {
+        if (i == 0)
+            return 0;
+        else if (i == 1)
+            return lobatto_fn_tab_1d[0](map_left(x));
+        else if (i % 2 == 1)
+            return 0;
+        else
+            return lobatto_fn_tab_1d[i/2](map_left(x));
+    } 
 }
