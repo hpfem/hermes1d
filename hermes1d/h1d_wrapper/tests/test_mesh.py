a5e3a34ea59d97b78cf845c54268af63961e9399
Ondrej Certik <ondrej@certik.cz>
2010-07-08 00:07:52 -0700

    More tests written

--- hermes1d/h1d_wrapper/tests/test_mesh.py
+++ hermes1d/h1d_wrapper/tests/test_mesh.py
@@ -10,9 +10,24 @@ def test_mesh1():
     assert (abs(pts - array([-1, -0.6, -0.2, 0.2, 0.6, 1])) < eps).all()
     assert (abs(p - array([2, 2, 2, 2, 2])) < eps).all()
 
+    m = Mesh(-1, 1, 4, 1)
+    pts, p = m.get_mesh_data()
+    assert (abs(pts - array([-1, -0.5, 0, 0.5, 1])) < eps).all()
+    assert (abs(p - array([1, 1, 1, 1])) < eps).all()
+
 def test_mesh2():
     m = Mesh(-1, 1, 5, 2, 1)
+    pts, p = m.get_mesh_data()
+    assert (abs(pts - array([-1, -0.6, -0.2, 0.2, 0.6, 1])) < eps).all()
+    assert (abs(p - array([2, 2, 2, 2, 2])) < eps).all()
 
 def test_mesh3():
-    m1 = Mesh([0, 1, 3, 4], [2, 2, 2])
-    m2 = Mesh([-1, -0.5, 0, 0.5, 1], [2, 2, 2, 2])
+    m = Mesh([0, 1, 3, 4], [2, 2, 2])
+    pts, p = m.get_mesh_data()
+    assert (abs(pts - array([0, 1, 3, 4])) < eps).all()
+    assert (abs(p - array([2, 2, 2])) < eps).all()
+
+    m = Mesh([-1, -0.5, 0, 0.5, 1], [2, 2, 2, 2])
+    pts, p = m.get_mesh_data()
+    assert (abs(pts - array([-1, -0.5, 0, 0.5, 1])) < eps).all()
+    assert (abs(p - array([2, 2, 2, 2])) < eps).all()
